58bbae2c2576c5c095d0437b017258cc3839a4e3
alexander-soko1ov <aleksandrsokol2602@gmail.com>
2024-08-13 23:59:46 +0300

    update release

--- test/tests_programs/test_lib_2/lib1/lib1.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace lib1 {
-    // Функция, принимающая два целых числа и возвращающая их сумму
-    std::string add(char *argv[]);
-}
--- test/tests_programs/test_lib_2/lib2/lib2.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace lib2 {
-    // Функция, принимающая число и выводящая его квадрат на консоль
-    void squareAndPrint(std::string number);
-}
